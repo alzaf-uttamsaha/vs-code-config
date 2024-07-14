e4808e6e922035d66564aa6c3bb898c0663534bf
Uttam Kumar Saha <155606564+alzaf-uttamsaha@users.noreply.github.com>
2024-07-14 16:57:48 +0600

    Create meta.js

---
+++ meta.js
@@ -0,0 +1,28 @@
+export const HomeMeta = {
+    title: '',
+    description: '',
+    keywords: "seperated by comma",
+    authors: {
+        name: '',
+        url: ""
+    },
+    robots: "index, follow",
+    openGraph: {
+        title: "",
+        description: "",
+        images: "image url",
+        url: "website url",
+        type: "website",
+        siteName: "sitename",
+    },
+    twitter: {
+        card: "summary_large_image",
+        title: "",
+        description: "",
+        images: "img url",
+        site: "@uttamsdev",
+    },
+    alternates: {
+        canonical: "https://website.url"
+    }
+};
